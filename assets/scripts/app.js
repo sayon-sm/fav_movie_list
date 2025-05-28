4624838306b7a47c2fa69ff004b649d1acda5774
sm <its.sayon.mitra@gmail.com>
2025-05-29 00:14:02 +0530

    adding cancel button functionality, renaming variable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,13 @@
 const addMovieButton = document.querySelector('header').lastElementChild;
-const addMovieModal = document.getElementById('add-modal');
+const movieModal = document.getElementById('add-modal');
+const movieModalAction = movieModal.querySelector('.modal__actions');
+const add = movieModalAction.firstElementChild;
 
-function movieModal() {
-  addMovieModal.classList.toggle('visible');
+console.log(add, movieModalAction, movieModal, addMovieButton);
+
+function toggleMovieModal() {
+  movieModal.classList.toggle('visible');
 }
 
-addMovieButton.addEventListener('click', movieModal);
-console.log(addMovieButton, addMovieModal);
+addMovieButton.addEventListener('click', toggleMovieModal);
+add.addEventListener('click', toggleMovieModal);
