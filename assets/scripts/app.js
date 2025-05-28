c534a2fe49df46507fd76af0abc3f40132398f9e
sm <its.sayon.mitra@gmail.com>
2025-05-29 00:42:16 +0530

    adding add button functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,15 @@
 const addMovieButton = document.querySelector('header').lastElementChild;
 const movieModal = document.getElementById('add-modal');
 const movieModalAction = movieModal.querySelector('.modal__actions');
-const add = movieModalAction.firstElementChild;
+const cancel = movieModalAction.firstElementChild;
+const add = movieModalAction.lastElementChild;
 
-console.log(add, movieModalAction, movieModal, addMovieButton);
+console.log(add, cancel, movieModalAction, movieModal, addMovieButton);
 
 function toggleMovieModal() {
   movieModal.classList.toggle('visible');
 }
 
 addMovieButton.addEventListener('click', toggleMovieModal);
+cancel.addEventListener('click', toggleMovieModal);
 add.addEventListener('click', toggleMovieModal);
