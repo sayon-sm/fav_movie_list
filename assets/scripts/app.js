bb281da188c793255d3702e99ec53b8fdc0106f7
sm <its.sayon.mitra@gmail.com>
2025-05-29 00:51:28 +0530

    adding backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,25 @@
+const backdrop = document.getElementById('backdrop');
 const addMovieButton = document.querySelector('header').lastElementChild;
 const movieModal = document.getElementById('add-modal');
 const movieModalAction = movieModal.querySelector('.modal__actions');
 const cancel = movieModalAction.firstElementChild;
 const add = movieModalAction.lastElementChild;
 
-console.log(add, cancel, movieModalAction, movieModal, addMovieButton);
+// console.log(
+//   backdrop,
+//   add,
+//   cancel,
+//   movieModalAction,
+//   movieModal,
+//   addMovieButton
+// );
 
 function toggleMovieModal() {
+  backdrop.classList.toggle('visible');
   movieModal.classList.toggle('visible');
 }
 
 addMovieButton.addEventListener('click', toggleMovieModal);
 cancel.addEventListener('click', toggleMovieModal);
 add.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
