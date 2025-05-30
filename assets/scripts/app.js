7090067815ebb2748eb7e7c7015d5d75ce951c6f
sm <its.sayon.mitra@gmail.com>
2025-05-31 00:14:40 +0530

    enhancing add button functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,7 @@ const movieModalAction = movieModal.lastElementChild;
 const cancel = movieModalAction.firstElementChild;
 const add = movieModalAction.lastElementChild;
 const addMovieButton = document.querySelector('header').lastElementChild;
+const renderMovie = document.getElementById('movie-list');
 
 console.log(
   backdrop,
@@ -15,17 +16,45 @@ console.log(
   movieModalAction,
   cancel,
   add,
-  addMovieButton
+  addMovieButton,
+  renderMovie
 );
 
-function toggleMovieModal() {
+function toggleBackdrop() {
   backdrop.classList.toggle('visible');
-  movieModal.classList.toggle('visible');
 }
 
-function getInput() {}
+function toggleMovieModal() {
+  movieModal.classList.toggle('visible');
+  toggleBackdrop();
+}
+
+function getInput() {
+  const userInput = {
+    text: input[0].value,
+    image: input[1].value,
+    rating: input[2].value,
+  };
+  displayInput(userInput);
+}
+
+function displayInput(userInput) {
+  const element = document.createElement('li');
+  element.className = 'movie-element';
+  element.innerHTML = `
+  <div class="movie-element__image">
+      <img src="${userInput.image}" alt="${userInput.text}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${userInput.text}</h2>
+      <p>${userInput.rating}/5 stars</p>
+    </div>
+  `;
+  renderMovie.appendChild(element);
+  toggleMovieModal();
+}
 
 addMovieButton.addEventListener('click', toggleMovieModal);
 cancel.addEventListener('click', toggleMovieModal);
-add.addEventListener('click', toggleMovieModal);
+add.addEventListener('click', getInput);
 backdrop.addEventListener('click', toggleMovieModal);
